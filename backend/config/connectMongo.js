b5059111530854a65a18581721a3fc514ec38d53
puriprajun@gmail.com <Prajunwooz>
2026-01-21 17:30:27 +0545

    teest

--- backend/config/connectMongo.js
+++ backend/config/connectMongo.js
@@ -5,7 +5,7 @@ dotenv.config();  //config is path  location
 export const connectDB = async() => {
 console.log(process.env.SECRET_KEY)
     try {
-        const result = await mongoose.connect(process.env.MONGOBD_URL);
+        const result = await mongoose.connect(process.env.MONGODB_URL);
 {result ? console.log("Mongo DB connected ") :console.log ("Mongo DB is not connected")}
     } catch (error) {
         console.log(error.message)
