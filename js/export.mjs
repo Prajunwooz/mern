825d09d2bdaece1508e024d0feed39ec6896ef8d
Prajun Puri <puriprajun@gmail.com>
2026-05-10 22:46:51 +0545

    Remove js and server folders

--- js/export.mjs
+++
@@ -1,4 +0,0 @@
-export const appleproduct=({productname,quantity}) => {
-    console.log(`Product Name: ${productname} | Quantity: ${quantity}`);
-    console.log("Apple product details exported successfully.");
-}
--- js/import.mjs
+++
@@ -1,3 +0,0 @@
-import { appleproduct } from "./export.mjs";
-
-appleproduct({productname:"iPhone 13",quantity:50});
