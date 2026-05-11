a52d48a369c2b6f3913de1b8ffa46814bf284ab7
Prajun Puri <puriprajun@gmail.com>
2026-05-11 15:43:18 +0545

    Fix register duplicate request and backend error

--- backend/config/connectMongo.js
+++ backend/config/connectMongo.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose"
-import dotenv from "dotenv";
+import { MONGODB_URL } from "./env.js"
+
   //config is path  location
 // dotenv.config({path:"../.env"});
 export const connectDB = async() => {
 // console.log(process.env.SECRET_KEY)
     try {
-        const result = await mongoose.connect(process.env.MONGODB_URL   ) //we can also write url directly but we use env package to we should import it;
-        console.log(process.env.MONGODB_URL )
+        const result = await mongoose.connect(MONGODB_URL) //we can also write url directly but we use env package to we should import it;
+        console.log( MONGODB_URL)
 {result ? console.log("Mongo DB connected ") :console.log ("Mongo DB is not connected")}
     } catch (error) {
         console.log(error.message)
