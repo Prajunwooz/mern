8d8a0953bb9f7e89538b8c94ffb52c0fb9eccf5b
Prajun Puri <puriprajun@gmail.com>
2026-05-15 00:09:07 +0545

    array

--- backend/utils/nodemailer.js
+++ backend/utils/nodemailer.js
@@ -163,7 +163,7 @@ export const sendMail = async ({ email, subject, html }) => {
 
         const result = await resend.emails.send({
             from: `Prajun Puri <${SMTP_EMAIL}>`,
-            to: email,
+            to: [email],
             subject,
             html,
         });
